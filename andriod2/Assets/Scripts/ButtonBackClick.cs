ecc8807b4101e4c6e7603195ca404cea613209ea
liubq7 <liubq97@gmail.com>
2021-02-08 17:54:01 +0800

    new project test

---
+++ andriod2/Assets/Scripts/ButtonBackClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBackClick : MonoBehaviour
+{
+    public Button button1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button1.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ andriod2/Assets/Scripts/CowTap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowTap : MonoBehaviour
+{
+    public GameObject cow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                if (Input.GetTouch(i).tapCount == 2)
+                {
+                    // cow.SetActive(false);
+                }
+            }
+        }
+    }
+}
