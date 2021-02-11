403b80e37d4d63591af8eea5920a9cef95903233
liubq7 <liubq97@gmail.com>
2021-02-11 14:11:12 +0800

    scenechange

---
+++ tangram/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChange : MonoBehaviour
+{
+    public Button mButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void sceneChange(int i)
+    {
+        Application.LoadLevel(i);
+    }
+}
