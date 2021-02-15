c35bd7d2dd889a1760ddec0e69729a825f83919d
liubq7 <liubq97@gmail.com>
2021-02-15 14:41:40 +0800

    button

---
+++ tangram/Assets/Scripts/ButtonChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonChange : MonoBehaviour
+{
+    public Button mButton;
+    public Sprite play;
+    public Sprite pause;
+    protected bool flag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // switch = false;
+        mButton.onClick.AddListener
+        (
+            delegate () { SetPause(); }
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetPause() {
+        if (flag) {
+            mButton.image.sprite = play;
+            flag = false;
+        } else {
+            mButton.image.sprite = pause;
+            flag = true;
+        }
+    }
+}
